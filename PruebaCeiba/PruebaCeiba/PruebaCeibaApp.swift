ea300a4124646eb0cfd5b6b0492900db47154f49
Sergio Rodriguez <sergiorodrigueztaborda@gmail.com>
2022-12-05 20:15:13 -0500

    Creacion de proyecto

---
+++ PruebaCeiba/PruebaCeiba/PruebaCeibaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaCeibaApp.swift
+//  PruebaCeiba
+//
+//  Created by Sergio Josue Rodriguez Taborda on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PruebaCeibaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
